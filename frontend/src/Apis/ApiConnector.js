cbb966f7816f1a1e28bb0e224d67d2533e538ab8
KarishmaGR <karishmasvryadav@gmail.com>
2024-04-10 01:45:19 +0530

    cors origin addedd

--- frontend/src/Apis/ApiConnector.js
+++ frontend/src/Apis/ApiConnector.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-const axiosInstance = axios.create({});
+const axiosInstance = axios.create({
+  baseURL: "https://lecture-sheduling-backend.onrender.com/api/v1", // Adjust the base URL as needed
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "http://localhost:5173", // Specify the exact origin
+  },
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
