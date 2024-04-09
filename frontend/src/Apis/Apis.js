5b4fba6035548a89aaf6161fd7e9fda751f3dbc4
KarishmaGR <karishmasvryadav@gmail.com>
2024-04-10 02:58:29 +0530

    cors error

--- frontend/src/Apis/Apis.js
+++ frontend/src/Apis/Apis.js
@@ -1,22 +1,22 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = "/api";
 
 export const USER_APIS = {
-  SIGN_UP_API: BASE_URL + "/user/createuser",
-  LOGIN_API: BASE_URL + "/user/loginuser",
-  LOGOUT_API: BASE_URL + "/user/logout",
-  GET_ALL_USER_API: BASE_URL + "/user/alluser",
+  SIGN_UP_API: "/user/createuser",
+  LOGIN_API: "/user/loginuser",
+  LOGOUT_API: "/user/logout",
+  GET_ALL_USER_API: "/user/alluser",
 };
 
 export const LECTURE_API = {
-  CREATE_LECTURE_API: BASE_URL + "/lecture/createlecture",
+  CREATE_LECTURE_API: "/lecture/createlecture",
 };
 
 export const GET_ALL_LECTURE_OF_INSTRUCTOR_API = {
-  GETALLLECTURE_API: BASE_URL + "/instructor/getalllectures",
+  GETALLLECTURE_API: "/instructor/getalllectures",
 };
 
 export const COURSE_API = {
-  CREATE_COURSE_API: BASE_URL + "/course/createcourse",
-  GET_ALL_COURSE: BASE_URL + "/course/getallcourse",
-  GET_SINGLE_COURSE: BASE_URL + "/course/getsiglecourse",
+  CREATE_COURSE_API: "/course/createcourse",
+  GET_ALL_COURSE: "/course/getallcourse",
+  GET_SINGLE_COURSE: "/course/getsiglecourse",
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxying requests starting with `/api` to the backend
+      "/api": {
+        target: "https://lecture-sheduling-backend.onrender.com/api/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
