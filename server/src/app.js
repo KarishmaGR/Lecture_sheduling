6a34d0b9e49c65ed3b4b489bc6f5436649d8ab71
KarishmaGR <karishmasvryadav@gmail.com>
2024-04-10 02:45:54 +0530

    Cors error:

--- server/src/app.js
+++ server/src/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: ["http://localhost:5173", "*"],
-    credentials: true,
+    credentials: false,
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
   })
 );
